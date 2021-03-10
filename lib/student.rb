a1e121d18afb997770cbab3b478adae452f7999d
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2021-03-10 23:39:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,20 +37,20 @@
   
 #   # ___________________________________________________
   
-  def save
-    if self.id
-      self.update
-    else
-      sql = <<-SQL
-      INSERT INTO students (name, grade) VALUES
-      (?, ?)
-      SQL
+  # def save
+  #   if self.id
+  #     self.update
+  #   else
+  #     sql = <<-SQL
+  #     INSERT INTO students (name, grade) VALUES
+  #     (?, ?)
+  #     SQL
     
-      DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid()
-      FROM students")[0][0]
-    end
-  end
+  #     DB[:conn].execute(sql, self.name, self.grade)
+  #     @id = DB[:conn].execute("SELECT last_insert_rowid()
+  #     FROM students")[0][0]
+  #   end
+  # end
   
 #   # ___________________________________________________
   
@@ -154,7 +154,7 @@ class Student
     
 #     DB[:conn].execute(sql, self.name, self.grade, self.id)
 #   end  
-# end
+end
 
 
 
