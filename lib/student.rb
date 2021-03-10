3442ac3438b38e2ce876f976234eeecf44320f0b
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2021-03-10 23:57:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -176,9 +176,9 @@ class Student
       SELECT * FROM students WHERE name = ?
       LIMIT 1
     SQL
-      DB[:conn].execute(sql, self.name).map do|row|
-      self.new_from_db(name)
-    end
+      DB[:conn].execute(sql, name).map do|row|
+        self.new_from_db(name)
+      end.first
   end  
     
 end
