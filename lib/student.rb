67b2706cb961005341a6ac299a021079685bf5a5
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2021-03-10 23:15:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -117,6 +117,14 @@ class Student
     @name = name
     @grade = grade
   end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)
+    SQL
+    
+    DB[:conn].execute(sql)
+  end    
 end
 
 
