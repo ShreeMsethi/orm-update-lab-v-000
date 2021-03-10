6b21c27705ed9ebf3ef84a2371008ae77af7c777
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2021-03-10 23:54:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -162,9 +162,9 @@ class Student
   end  
   
   def self.new_from_db(row)
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
+    id = row[0]
+    name = row[1]
+    grade = row[2]
     
     student = self.new(name, grade)
     student.id = id
