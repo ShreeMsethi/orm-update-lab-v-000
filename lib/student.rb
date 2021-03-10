3a6db74e6cf7443f7dbe24d5bea0d030db4d8b12
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2021-03-10 23:35:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,13 +27,13 @@
   
 # # ___________________________________________________
  
-  def self.drop_table
-    sql = <<-SQL
-    DROP TABLE students
-    SQL
+  # def self.drop_table
+  #   sql = <<-SQL
+  #   DROP TABLE students
+  #   SQL
     
-    DB[:conn].execute(sql)
-  end
+  #   DB[:conn].execute(sql)
+  # end
   
 #   # ___________________________________________________
   
@@ -126,25 +126,25 @@ class Student
     DB[:conn].execute(sql)
   end 
   
-#   def self.drop_table
-#     sql = <<-SQL
-#       DROP TABLE IF EXISTS students
-#     SQL
-#     DB[:conn].execute(sql)
-#   end  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+    SQL
+    DB[:conn].execute(sql)
+  end  
   
-#   def save
-#     if self.id 
-#       self.update
-#     else
-#     sql = <<-SQL
-#       INSERT INTO students (name, grade)
-#       VALUES (?, ?)
-#       SQL
-#       DB[:conn].execute(sql, self.name, self.grade)
-#       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-#     end
-#   end 
+  def save
+    if self.id 
+      self.update
+    else
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end 
   
 #   def update 
 #     sql = <<-SQL
